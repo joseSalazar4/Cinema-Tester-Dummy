a96998f379914248757ac5d846bec8db6c9a8421
Jose <jsalazarg48@gmail.com>
2021-10-21 08:28:46 -0600

    Fixed movie Link usage

--- front-end/src/components/CardItem.js
+++ front-end/src/components/CardItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button'
+import { Link } from 'react-router-dom';
 import MyVerticallyCenteredModal from '../components/Modal.js'
 
 function CardItem(props) {
@@ -13,14 +14,14 @@ function CardItem(props) {
         movieTitle= {props.text}
         movieType= {props.label}/>
       <li className='cards__item'>
-        <Button className='cards__item__link' onClick={() => setModalShow(true)}>
+        <label className='cards__item__link' onClick={() => setModalShow(true)} >
           <figure className='cards__item__pic-wrap' data-category={props.label}>
             <img className='cards__item__img' alt='Travel' src={props.src} />
           </figure>
           <div className='cards__item__info'>
             <h5 className='cards__item__text'>{props.text}</h5>
           </div> 
-        </Button>
+        </label>
       </li>
     </>
   );
