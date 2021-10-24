2e22186b64936e2fde136a9d49a9ffb9b9efd543
dcastro18 <h.davidcastro14@gmail.com>
2021-10-24 13:19:21 -0600

    Payment Form added
    The payment form is added but its not finished

--- front-end/src/components/pages/SignUp.js
+++ front-end/src/components/pages/SignUp.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../../App.css';
-import Payment from '../Payment';
+import PaymentForm from '../PaymentForm';
 
 export default function SignUp() {
-  return <Payment />
+  const adminUser = {
+    email:"admin@gmai.com",
+    password: "1234"
+  }
+
+  const [user, setUser] = useState({name: "", email:""});
+  const [error, setError] = useState("");
+
+  const Login = detais => {
+      console.log(detais);
+  }
+
+  const Logout = () =>{
+      console.log("Log out")
+  }
+
+  return (
+    <div className="Payment">
+        {(user.email != "") ? (
+            <div className="welcome">
+                <h2>Welcome, <span>{user.name}</span></h2>
+                <button>logout</button>
+            </div>
+        ) : (
+          <PaymentForm Login={Login} error={error} />
+        )}
+    </div>
+);
 }
