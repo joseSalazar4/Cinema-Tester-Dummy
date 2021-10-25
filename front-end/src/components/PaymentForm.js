826cbc11c75dc17da77de44a9958208459575c16
dcastro18 <h.davidcastro14@gmail.com>
2021-10-25 16:41:03 -0600

    Back conection

--- front-end/src/components/PaymentForm.js
+++ front-end/src/components/PaymentForm.js
@@ -8,51 +8,44 @@ function PaymentForm({ SendInfo, info }) {
     const seatsArray = info.seatsArray;
     const total = info.total;
 
-
     const [details, setDetails] = useState({title: movieTitle, seats: seatsArray, total: total, name:"", email:""});
 
     const submitHandler = e => {
         e.preventDefault();
-
         SendInfo(details);
+
     }
 
-    function NumberList(props) {
-        const numbers = props;
-        const listItems = numbers.map((number) =>
-          <li>Asiento: {number}</li>
-        );
-        return (
-          <ul>{listItems}</ul>
-        );
-      }
+    // function NumberList(props) {
+    //     const numbers = props;
+    //     const listItems = numbers.map((number) =>
+    //       <li>Asiento: {number}</li>
+    //     );
+    //     return (
+    //       <ul>{listItems}</ul>
+    //     );
+    //   }
 
-
-
-    console.log(seatsArray) 
     return (
-        <div className="paymentForm">
-            
-            <form onSubmit={submitHandler} >
-                <div className="form-inner">
-                    {/* ERROR */}
-                    <div className="form-group">
-                        <label htmlFor="name">Nombre: </label>
-                    </div>
-                        <input type="text" name="name" id="name" onChange={e => setDetails({...details, name: e.target.value})} value={details.name}/>
-                    <div className="form-group">
-                        <label htmlFor="email">Correo Electronico: </label>
-                    </div>
-                        <input type="email" name="email" id="email" onChange={e => setDetails({...details, email: e.target.value})} value={details.email}/>
+        <form onSubmit={submitHandler} >
+            <div className="form-inner">
+                {/* ERROR */}
+                <div className="form-group">
+                    <label htmlFor="name">Nombre: </label>
+                    <input type="text" name="name" id="name" onChange={e => setDetails({...details, name: e.target.value})} value={details.name}/>
                 </div>
-            </form>
-
-            <h5>Película: {details.movieTitle}</h5>
-            <h5>Asientos seleccionados</h5>
-            {NumberList(seatsArray)} 
-            <h5>Total a pagar: {total} colones</h5>
+                <div className="form-group">
+                    <label htmlFor="email">Correo Electronico: </label>
+                    <input type="email" name="email" id="email" onChange={e => setDetails({...details, email: e.target.value})} value={details.email}/>
+                </div>
+            </div>
             <input type="submit" value="Comprar" />
-        </div>
+            {/* <h5>Película: {details.movieTitle}</h5>
+            <h5>Asientos seleccionados</h5>
+            {NumberList(seatsArray)}
+            <h5>Total a pagar: {total} colones</h5> */}
+            
+        </form>
     );
 }
   
